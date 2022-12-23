77c82cc355304214c60cf0e4aa58d3310f79cc17
GRohithSai <rohithsai@brightcomgroup.com>
2022-12-23 11:31:38 +0000

    Yes

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3="[0,3]"
+word_first_3="Hol"
+word_last_2="on"
+Middle word="olberton"
 print("First 3 letters: {}".format(word_first_3))
-word_last_2="[-1:]"
 print("Last 2 letters: {}".format(word_last_2))
-middle_word="[1:8]"
 print("Middle word: {}".format(middle_word))
