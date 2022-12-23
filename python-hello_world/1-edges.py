732367d56779f7e5f675a325f40e44d5f4e9244e
GRohithSai <rohithsai@brightcomgroup.com>
2022-12-23 11:13:39 +0000

    True

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-
+def word_first_3:
 print("First 3 letters: {}".format(word_first_3))
-
+def word_last_2:
 print("Last 2 letters: {}".format(word_last_2))
-
+def middle_word:
 print("Middle word: {}".format(middle_word))
